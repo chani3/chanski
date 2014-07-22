cd98473083be80f4c94ccb14f74d3f1209ba2bb9
Chani Armitage <chanika@gmail.com>
2014-07-21 20:32:31 -0700

    Use ejs or hello-world

--- node/app.js
+++ node/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('hello');
 });
 
 var server = app.listen(3000, function() {

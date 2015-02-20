8c2f65ad843c20cf97c31d623cc34ece138644a9
Chani Armitage <chanika@gmail.com>
2015-02-20 12:18:06 -0800

    test the certificate, and post
    the post test probably ought to be a separate file, later.

--- node/tests/ssl.sh
+++ node/tests/ssl.sh
@@ -3,6 +3,15 @@
 #TODO: a testing framework would be nice.
 
 url="https://chani.ca:3000"
+ssl="--cacert curl-ca-bundle.crt"
+sslUrl="$ssl $url"
 
 echo testing insecure https...
 curl -k $url
+
+echo testing certificate...
+curl $sslUrl
+
+echo testing post...
+curl -H "Content-Type: application/json" -d'{"a":"b"}' $sslUrl
+

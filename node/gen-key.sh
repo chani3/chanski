3285fc6ed19f179c4c30d9e50529c39d4f12c90c
Chani Armitage <chanika@gmail.com>
2015-02-20 12:07:20 -0800

    Add public cert to repo, for easy use on the other side.

--- node/gen-key.sh
+++ node/gen-key.sh
@@ -3,12 +3,13 @@
 
 sslDir='keys'
 
-key=$sslDir/key.pem
-request=$sslDir/request.csr
-cert=$sslDir/cert.pem
+key=$sslDir/key.pem #private key
+request=$sslDir/request.csr #signing request
+cert=$sslDir/cert.pem #public cert
 
 mkdir -p $sslDir
 openssl genrsa -out $key 1024 
 openssl req -new -key $key -out $request
 openssl x509 -req -in $request -signkey $key -out $cert
+cp -i $cert curl-ca-bundle.crt #expose public cert to tests
 

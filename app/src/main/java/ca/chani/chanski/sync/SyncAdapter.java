2df6826179e424a2e2d0e5c603bc0f874b6bcc4e
Chani Armitage <chanika@gmail.com>
2015-02-20 15:13:58 -0800

    use the self-signed ssl cert to connect to the server.
    it works! :) :) #2
    there's no auth yet, though.

--- app/src/main/java/ca/chani/chanski/sync/SyncAdapter.java
+++ app/src/main/java/ca/chani/chanski/sync/SyncAdapter.java
@@ -15,13 +15,26 @@ import com.loopj.android.http.SyncHttpClient;
 import com.loopj.android.http.TextHttpResponseHandler;
 
 import org.apache.http.Header;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import ca.chani.chanski.R;
 
 /**
  * Created by chani on 2014-03-03.
@@ -79,17 +92,23 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
         Log.e(TAG, String.format("%d things changed", updates));
         if (updates <= 0) {
-            return;
+            //return;
         }
 
         JSONObject postData = new JSONObject();
         postData.put("data", dataArray);
         Log.e(TAG, "json: " + postData.toString());
 
-        httpClient = new SyncHttpClient();
+        httpClient = new SyncHttpClient(); //note: don't bother specifying port here, it's lost when we set up the cert
+        //TODO maybe we should keep the httpclient around to reuse this? or at least the ssl factory?
+        try {
+            addSslCert(httpClient);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         httpClient.addHeader("Authorization", token);
 
-        String url = "https://chani.ca/TODO";
+        String url = "https://chani.ca:3000/";
         StringEntity entity = new StringEntity(postData.toString());
         String contentType = "application/json";
         httpClient.post(getContext(), url, entity, contentType, new TextHttpResponseHandler() {
@@ -104,6 +123,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseBody, Throwable error) {
                 Log.e(TAG, "failure: " + error.toString());
+                error.printStackTrace();
                 Log.e(TAG, Integer.toString(statusCode));
                 if (statusCode == 401) {
                     syncResult.stats.numAuthExceptions++;
@@ -145,5 +165,35 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         //TODO serialize the item
 
         return jsonObject;
+    }
+
+    /**
+     * this function is 90% example code.
+     * it's a hacky merger of https://developer.android.com/training/articles/security-ssl.html and
+     * https://github.com/loopj/android-async-http/blob/master/sample/src/main/java/com/loopj/android/http/sample/CustomCASample.java
+     * with my own shortcuts when I noticed I can just pass the keystore to apache's SSLSocketFactory instead of javax's.
+     * I really hope this works properly.
+      */
+    private void addSslCert(SyncHttpClient httpClient) throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException, UnrecoverableKeyException {
+
+        // Load CAs from an InputStream
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream caInput = getContext().getResources().openRawResource(R.raw.cert);
+        Certificate ca;
+        try {
+            ca = cf.generateCertificate(caInput);
+            Log.d(TAG, "ca=" + ((X509Certificate) ca).getSubjectDN());
+        } finally {
+            caInput.close();
+        }
+
+        // Create a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        SSLSocketFactory factory = new SSLSocketFactory(keyStore);
+        httpClient.setSSLSocketFactory(factory);
     }
 }
